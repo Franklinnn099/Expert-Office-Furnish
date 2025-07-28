44d1c898e3d793dc2a2a6271f318449a8b14223c
Franklin <bahdrhymez123@gmail.com>
2025-07-28 09:46:35 +0000

    edit button fix

--- dashboard-app/src/pages/Products.jsx
+++ dashboard-app/src/pages/Products.jsx
@@ -69,7 +69,7 @@ export default function Products() {
               onClick={() => navigate("/edit-product/1")}
               className="mt-2 px-4 py-2 bg-green-700 text-white rounded hover:bg-green-800"
             >
-              Edit First Product
+              Edit Product
             </button>
           </div>
         </div>
